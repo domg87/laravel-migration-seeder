6a0d7ddadce36b230b6cdb345df1fcc988b364c6
dom <domepiro87@yahoo.it>
2020-11-02 16:48:46 +0100

    create_seeder+controller

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -11,8 +11,23 @@ class ComicsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 30; $i++) {
+
+            $newComic = new Comic();
+            $newComic->title = $faker->text(30);
+            $newComic->original_title = $faker->text(50);
+            $newComic->author = $faker->name();
+            $newComic->number = $faker->numberBetween(1, 1000);
+            $newComic->n_page = $faker->numberBetween(50, 400);
+            $newComic->edition = $faker->company();
+            $newComic->price = $faker->randomFloat(2, 1, 1000);
+            $newComic->color = rand(0,1);
+            $newComic->release = $faker->year();
+            $newComic->cover = $faker->imageUrl($width = 150, $height = 300);
+
+            $newComic->save();
+        }
     }
 }

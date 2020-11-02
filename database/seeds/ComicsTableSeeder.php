75bbc1e3435a7235994db0314f4be14df4ab4584
dom <domepiro87@yahoo.it>
2020-11-02 16:04:01 +0100

    create_table

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+use Faker\Generator as Faker; 
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
